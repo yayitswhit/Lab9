6384cda9b1c4f3144794d7df3422b9b730a8cd4b
yayitswhit <104163475+yayitswhit@users.noreply.github.com>
2023-02-23 17:21:45 -0500

    first draft

---
+++ Lab9/Lab9/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Lab9
+//
+//  Created by Gilbert W. Sanborn Jr on 2/23/23.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    ofstream outFile;
+    ifstream inFile;
+    int userNumber;
+    
+    outFile.open("/Users/whitsanborn/Documents/Gateway/Programming with Object Oriented C++/Lab9/Lab9/input.txt");
+    inFile.open("/Users/whitsanborn/Documents/Gateway/Programming with Object Oriented C++/Lab9/Lab9/input.txt");
+
+    do
+    {
+        cout << "Input a number: ";
+        cin >> userNumber;
+        outFile << userNumber << endl;
+        
+    }
+    while(userNumber >= 0);
+    
+/*    double fileNumber;
+    while(!inFile.eof()) {
+        inFile >> fileNumber;
+        cout << fileNumber << endl;
+    }*/
+    return 0;
+}
